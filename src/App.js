12e27c0f6c02b860bf130b0276d4fffc2819aaa7
AmitPandey45 <amitpandeymca30@gmail.com>
2020-08-22 12:00:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    // pending > resolved (success) OR rejected (failure)
+    const { data: posts } = await axios.get(apiEndpoint);
+    this.setState({ posts });
+  }
+
+  handleAdd = async () => {
+    const obj = { title: "a", body: "b" };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
+  };
+
+  handleUpdate = async (post) => {
+    post.title = "Updated title!";
+    await axios.put(apiEndpoint + "/" + post.id, post);
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(post);
+    posts[index] = { ...post };
+    this.setState({ posts });
+  };
+
+  handleDelete = async (post) => {
+    const originalPosts = [...this.state.posts];
+
+    const posts = originalPosts.filter((s) => s.id !== post.id);
+    this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndpoint + "//9999" + post.id);
+      throw new Error("");
+    } catch (ex) {
+      // Expected (404: not found, 400: bad request) - CLIENT ERRORs
+      //  - Display specific error message
+      //
+      // Unexpected (network down, server down, db down, bug)
+      //  - Log them
+      //  - Display generic and friendly error message
+      if (ex.response && ex.response.status === 404) {
+        alert("This post has already been deleted.");
+      } else {
+        console.log("Logging the error", ex);
+        alert("An expected error occurred.");
+      }
+      this.setState({ posts: originalPosts });
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button className="btn btn-primary" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((post) => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
